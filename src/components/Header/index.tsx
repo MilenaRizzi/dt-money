873accbcf1e1d2df1fc088a2e54e52a5be69ef0b
Milena Clara Rizzi <116590085+MilenaRizzi@users.noreply.github.com>
2024-02-27 14:11:25 -0300

    uso do eslint

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,7 @@
-import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles'
 import * as Dialog from '@radix-ui/react-dialog'
 import logoImg from '../../assets/logo.svg'
-import { NewTransactionModal } from "../NewTransactionModal";
+import { NewTransactionModal } from '../NewTransactionModal'
 export function Header() {
   return (
     <HeaderContainer>
@@ -11,7 +11,7 @@ export function Header() {
           <Dialog.Trigger asChild>
             <NewTransactionButton>Nova transação</NewTransactionButton>
           </Dialog.Trigger>
-          <NewTransactionModal/>
+          <NewTransactionModal />
         </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
